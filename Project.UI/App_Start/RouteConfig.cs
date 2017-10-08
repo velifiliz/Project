287eee712d6dfaa7039ad86eb71694d72df711df
Veli Filiz <velifiliz@mail.com>
2017-10-08 07:04:18 +0300

    Test

--- Project.UI/App_Start/RouteConfig.cs
+++ Project.UI/App_Start/RouteConfig.cs
@@ -16,7 +16,6 @@ namespace Project.UI
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
