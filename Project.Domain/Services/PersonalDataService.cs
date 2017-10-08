91166c3420a4a51f361887aed66a0dce45b56b2e
Veli Filiz <velifiliz@mail.com>
2017-10-08 09:55:50 +0300

    Domain Katmanı

--- Project.Domain/Services/PersonalDataService.cs
+++ Project.Domain/Services/PersonalDataService.cs
@@ -5,6 +5,7 @@
         public int InsertCommand(Personal p)
         {
             string query = "INSERT INTO Personal VALUES (@Name,@Lastname)";
+
             return ExecuteCommandText(query,p);
         }
     }
